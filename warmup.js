e6bc5600f6649d81b09b387f06e7e25b18ec7348
debbie <drosenfeld87@gmail.com>
2018-03-21 13:19:12 -0400

    first 2 code block conversions are working

---
+++ warmup.js
@@ -0,0 +1,22 @@
+// var apples = 14;
+// console.log(apples);
+// console.log('I have ' + (apples) + ' ' + 'apples');
+
+// var materials  = ['wood', 'metal', 'stone'];
+// var words = {
+//   'elephant': "The world's largest land mammal.",
+//   'school': 'A place of learning.',
+//   'ice cream': 'A delicious milk-based dessert.'
+// };
+
+var num = 16;
+
+  if (num > 10) {
+    console.log(num  + ' is greater than 10.');
+  }
+  else if (num === 10) {
+    console.log(num  + ' is exactly 10.');
+  }
+  else {
+    console.log(num  + ' must be less than 10');
+  }
---
+++ warmup.rb
@@ -0,0 +1,20 @@
+# apples = 14
+# puts apples
+#
+# puts "I have #{apples} apples."
+#
+# materials = ['wood', 'metal', 'stone']
+# words = {
+#   'elephant' => "The world's largest land mammal.",
+#   'school' => 'A place of learning.',
+#   'ice cream' => 'A delicious milk-based dessert.',
+# }
+
+num = 16
+if num > 10
+  puts "#{num} is greater than 10."
+elsif num == 10
+  puts "#{num} is exactly 10."
+else
+  puts "#{num} must be less than 10."
+end
