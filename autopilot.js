70cba945839cb4714ab3a05e9164115b291e8f83
debbie <drosenfeld87@gmail.com>
2018-03-21 15:35:55 -0400

    started autopilot exercise

---
+++ autopilot.js
@@ -0,0 +1,33 @@
+var cars = [];
+
+function getNewCar() {
+  var new_car =  {
+      city: 'Toronto',
+      passengers: 0,
+      gas: 100,
+    };
+    return new_car
+  }
+
+function addCar(cars, newCar) {
+  cars.push(newCar);
+  console.log('Adding new car to fleet. Fleet size is now ' + cars.length );
+  }
+
+function pickUpPassenger(car) {
+  car.passengers += 1;
+  car.gas -=10;
+  console.log("Picked up passenger. Car now has " + car.passengers + " passengers" );
+}
+
+function getDestination(car) {
+  if (car.city === 'Toronto') {
+    return 'Mississauga'
+  }
+  else if (car.city === 'Mississauga') {
+    return 'London'
+  }
+  else if (car.city === 'London') {
+    return 'Toronto'
+  }
+}
